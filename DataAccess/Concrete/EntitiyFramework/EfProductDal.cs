6af347cd4437065cdd6979a7a168447044334dd6
burhan sozer <you@example.com>
2022-03-18 00:16:26 +0300

    first commit

---
+++ DataAccess/Concrete/EntitiyFramework/EfProductDal.cs
@@ -0,0 +1,12 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntitiyFramework.Contexts;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntitiyFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>, IProductDal
+    {
+
+    }
+}
